fb9ac24c33a5e1b4d8729993a7c88205202272ac
Achuthan24r <achuabi2419@gmail.com>
2024-10-12 17:31:20 +0530

    Update test.py
    testing of eligiblty

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-str1=input("enter a 6 digit code")
-str2=input("enter a rational number")
-str3=str1+str2
-
-
-
-
-
+person = int(input("enter your name: "))
+id=int(input("enter your id :"))
+profossion =int(input("enter your job:"))
+age= int (input("enter your age:"))
+p1=("person","id","prefossion")
+if p1 < age +29:
+   print("you are elligibal for this company")
+else:
+   print ("you are no not elligibal")
